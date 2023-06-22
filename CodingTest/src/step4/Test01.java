34061cce44ca00d003c03f77a23214a4fd76b3cf
chopoo2001 <chopoo2001@gmail.com>
2023-06-22 17:46:35 +0900

    20230622 오후 백준

--- CodingTest/src/step4/Test01.java
+++ CodingTest/src/step4/Test01.java
@@ -4,19 +4,28 @@ import java.util.Scanner;
 
 public class Test01 {
 	public static void main(String[] args) {
-		
+
 		Scanner sc = new Scanner(System.in);
-		
+
 		int n = sc.nextInt();	//정수 개수
-		int a = sc.nextInt();	//정수
-		int v = sc.nextInt();
 		int[] count = new int[n];
 
 		for (int i=0; i< n; i++) {
-			count[i] = a;
+			count[i] = sc.nextInt();
 		}
-		
-		int length = count.length;
-		
+
+		int v = sc.nextInt();
+		int num = 0;
+
+		for(int i=0; i<n; i++) {
+			if(count[i] == v) {
+				num++;
+			}
+
+		}
+
+		System.out.println(num);
+
+		 sc.close();
 	}
 }
