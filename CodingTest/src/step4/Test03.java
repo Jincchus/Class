4826179c790333650811b7cfb8cedd7208389b71
chopoo2001 <chopoo2001@gmail.com>
2023-06-27 12:26:42 +0900

    20230626 codingTest 문제 풀이

--- CodingTest/src/step4/Test03.java
+++ CodingTest/src/step4/Test03.java
@@ -1,7 +1,37 @@
 package step4;
 
+import java.util.Scanner;
+
 public class Test03 {
 	public static void main(String[] args) {
-		
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		int arr[] = new int[n];
+
+
+		int max = arr[0];
+		int min = arr[0];
+
+		for(int i = 0; i<n; i++) {
+			int a = sc.nextInt();
+			arr[i] = a;
+
+			if(i == 0) {
+				max = a;
+				min = a;
+			} else {
+				if(a < min) {
+					min = a;
+				} 
+				if (a > max) {
+					max = a;
+				}
+
+			}
+		}
+
+		System.out.print(min + " ");
+		System.out.println(max);
 	}
 }
--- CodingTest/src/step4/Test05.java
+++ CodingTest/src/step4/Test05.java
@@ -1,7 +1,23 @@
 package step4;
 
+import java.util.Scanner;
+
 public class Test05 {
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		
+		int arr[] = new int[n];
+		
+		for(int i=0 ; i<n; i++) {
+			int start = sc.nextInt();
+			int end = sc.nextInt();
+			int num = sc.nextInt();
+			
+		}
+		
 		
 	}
 }
