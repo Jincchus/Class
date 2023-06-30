73e426d84f74b9ed75ff2af5c25e584dafeab9b0
chopoo2001 <chopoo2001@gmail.com>
2023-06-30 12:23:29 +0900

    29일 보충수업, 연습문제

--- CodingTest/src/step4/Test06.java
+++ CodingTest/src/step4/Test06.java
@@ -4,40 +4,38 @@ import java.util.Scanner;
 
 public class Test06 {
 	public static void main(String[] args) {
-		
+
 		Scanner sc = new Scanner(System.in);
-		
+
 		int bucket = sc.nextInt();
 		int change = sc.nextInt();
-		
-		int[] buckets = new int[bucket];
-		
+
+		int[] buckets = new int[bucket +1];
+
 		int buc1 = 0;
 		int buc2 = 0;
-		
-		int[] temps = new int[0];
-		
-		for(int i=1 ; i<=change ; i++) {
+
+		for(int i=1 ; i<=bucket ; i++) {
+
+			buckets[i] =i; 
+		}
+
+		for(int i=0; i<change; i++) {
 			buc1 = sc.nextInt();
 			buc2 = sc.nextInt();
-			
-			buckets[i] =i; 
-	
-		}
-		
-		
-		
-		for(int j=0; j<bucket; j++) {
-			
-			temps[j] = buckets[buc1];
+
+
+
+			int temp = buckets[buc1];
 			buckets[buc1] = buckets[buc2];
-			buckets[buc2] = temps[j];
+			buckets[buc2] = temp;
+
 		}
-		
-		System.out.print(buckets[change] + " ");
-		
-		
-		
-		
+
+		for(int i = 1; i<=bucket; i++) {
+
+			System.out.print(buckets[i] + " ");
+		}
+
 	}
 }
--- CodingTest/src/step4/Test07.java
+++ CodingTest/src/step4/Test07.java
@@ -1,7 +1,45 @@
 package step4;
 
+import java.util.Scanner;
+
 public class Test07 {
 	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		/* 1. 학생 30명
+		 * 2. 각 학생별로 1부터 30까지 번호
+		 * 3. 과제 28명 제출, 제출 안 한 사람 2명
+		 * 4. 입력 28개 한줄씩 다 받기
+		 * 5. 안한 사람 2명, 출석 번호 중 작은것부터 출력됨
+		 */
+		
+		int student = 30;
+		int students[] = new int[student+1]; //1
+		
+		int work =0 ;
+		int student2 = 0;
+		int donwork[] = new int[student2+1];
+		
+		for(int i=0; i<=student; i++ ) {
+			students[i] = i;
+		}
+		
+		
+		
+		for(int i=0; i<student2-1; i++) {
+			work = sc.nextInt();
+			if (students[i]!=work) {
+				donwork[student2] = students[i];
+				student2++;
+			}
+		}
+		
+		for (int i = 0; i<student2-1;i++) {
+			System.out.println(donwork[i]+" ");
+		}
+		
+		
+		
 		
 	}
 }
