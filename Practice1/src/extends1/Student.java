3681eb2c88976d2119d430711bcce198d7cca5c7
chopoo2001 <chopoo2001@gmail.com>
2023-07-04 11:42:42 +0900

    20230704 람다식, 스트림 수업

---
+++ Practice1/src/extends1/Student.java
@@ -0,0 +1,12 @@
+package extends1;
+
+//3. Student
+//- 학생을 의미하는 클래스입니다.
+//- Teacher클래스를 상속받는 자식 클래스입니다.
+//- 메인메서드가 이곳에 존재하므로 프로그램의 시작은 Student 클래스입니다.
+//- 학생의 이름을 입력받아 Console화면에 결과물을 출력합니다.
+//- School -> Teacher -> Student 순으로 상속을 주고 받습니다.
+
+public interface Student extends Teacher {
+
+}
