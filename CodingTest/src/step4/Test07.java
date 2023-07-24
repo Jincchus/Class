8f7aa43738c967f3d7df093477076ff74ce42054
chopoo2001 <chopoo2001@gmail.com>
2023-07-24 17:41:14 +0900

    20230724 백준 연습문제

--- CodingTest/src/step4/Test07.java
+++ CodingTest/src/step4/Test07.java
@@ -1,4 +1,10 @@
 package step4;
+/* 1. 학생 30명
+ * 2. 각 학생별로 1부터 30까지 번호
+ * 3. 과제 28명 제출, 제출 안 한 사람 2명
+ * 4. 입력 28개 한줄씩 다 받기
+ * 5. 안한 사람 2명, 출석 번호 중 작은것부터 출력됨
+ */
 
 import java.util.Scanner;
 
@@ -6,39 +12,20 @@ public class Test07 {
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
-		/* 1. 학생 30명
-		 * 2. 각 학생별로 1부터 30까지 번호
-		 * 3. 과제 28명 제출, 제출 안 한 사람 2명
-		 * 4. 입력 28개 한줄씩 다 받기
-		 * 5. 안한 사람 2명, 출석 번호 중 작은것부터 출력됨
-		 */
 		
 		int students[] = new int[30]; //1
+		int don ;
 		
-		int report = 0 ;
-		int compStu = 28 ;
-		int donRep[] = new int[compStu];
 		
-		for(int i=0; i<=students.length; i++ ) {
-			students[i] = i+1;
+		for(int i=1; i<=28; i++ ) {
+			int report = sc.nextInt();
+			students[report-1] = 1;
 		}
-//		
-//		
-//		
-//		for(int i=0; i<student2-1; i++) {
-//			work = sc.nextInt();
-//			if (students[i]!=work) {
-//				donwork[student2] = students[i];
-//				student2++;
-//			}
-//		}
-//		
-//		for (int i = 0; i<student2-1;i++) {
-//			System.out.println(donwork[i]+" ");
-//		}
-//		
-//		
-//		
-		
+
+		for(int i=0 ; i<30; i++) {
+			if(students[i] == 0) {
+				System.out.println(i+1);
+			}
+		}
 	}
 }
