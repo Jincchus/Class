240400bc90a878030ec78112772a00d4e98befe7
Jincchus <chopoo2001@gmail.com>
2023-11-08 14:19:04 +0900

    20231108 vue component

---
+++ ch03/src/components/LifeComponent.vue
@@ -0,0 +1,54 @@
+<script>
+export default {
+  name: "LifeComponent",
+  methods: {
+    btnAdd: function () {
+      this.count++;
+    },
+    btnMin: function () {
+      this.count--;
+    },
+  },
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  // 컴포넌트 라이프 사이클 함수(Hook)
+  beforeCreate: function () {
+    console.log("beforeCreate...");
+  },
+  created: function () {
+    console.log("created...");
+  },
+  beforeMount: function () {
+    console.log("beforeMount...");
+  },
+  mounted: function () {
+    console.log("mounted...");
+  },
+  beforeUpdate: function () {
+    console.log("beforeUpdate...");
+  },
+  updated: function () {
+    console.log("updated...");
+  },
+  beforeUnmount: function () {
+    console.log("beforeUnmount...");
+  },
+  unmounted: function () {
+    console.log("unmounted...");
+  },
+};
+</script>
+
+<template>
+  <h3>LifeComponent</h3>
+  <p>
+    카운터 : {{ count }}
+    <button @click="btnAdd">증가</button>
+    <button @click="btnMin">감소</button>
+  </p>
+</template>
+
+<style scoped></style>
